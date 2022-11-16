c3ed07290341af4f647573488f44d2d45a69d9eb
Aaron James <plastikjames@gmail.com>
2022-11-16 20:05:14 +1300

    Making a start

---
+++ src/features/quote/quote.js
@@ -0,0 +1,9 @@
+const Quote = () => {
+    return (
+        <div id="quote">
+            <h2>This is for the quote</h2>
+        </div>
+    )
+}
+
+export default Quote;
---
+++ src/features/weather.js
@@ -0,0 +1,9 @@
+const Weather = () => {
+    return (
+        <div id="weatherbox">
+            <h2>This is for the weather</h2>
+        </div>
+    )
+}
+
+export default Weather;
